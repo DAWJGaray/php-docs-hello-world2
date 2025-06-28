b4047c10ec5a529dd76cb7c3c4218fb703488f42
cibelesmora <cibelesmora@gmail.com>
2025-06-28 08:36:58 +0200

    Add files via upload

---
+++ pdfProces.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gestor de arxius PDF Sistemes Informàtics</title>
+</head>
+<body>
+    <h1>Apunts Sistemes Informàtics</h1>
+    <h2><a href="https://almacenclasecm.blob.core.windows.net/pdfcontainercm" target="blank">PDFs originals</a>(valencià)</h2>
+    <ul>
+        <li>Ejemplo</li>
+    </ul>
+    <hr>
+    <h2><a href="https://almacenclasecm.blob.core.windows.net/textcontainercm" target="blank">Fitxers editables</a>(valencià)</h2>
+    <ul>
+        <li>Ej</li>
+    </ul>
+    <hr>
+    <h2><a href="https://almacenclasecm.blob.core.windows.net/translatedcontainercm" target="blank">PDFs traduïts</a>(castellà)</h2>
+    <ul>
+        <li>Ej</li>
+    </ul>
+</body>
